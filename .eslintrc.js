4cd7774b2fc8c5c926792e4e7606bf9052c795cf
Alex J Burke <alex@alexjeffburke.com>
2020-02-09 19:17:11 +0100

    Standardise on two space indent.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    extends: ['standard', 'prettier', 'prettier/standard'],
-    env: {
-        amd: true,
-        es6: true
-    },
-    overrides: [
-        {
-            files: ['test/*.spec.js'],
-            env: {
-                mocha: true
-            }
-        }
-    ]
+  extends: ['standard', 'prettier', 'prettier/standard'],
+  env: {
+    amd: true,
+    es6: true
+  },
+  overrides: [
+    {
+      files: ['test/*.spec.js'],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 };
--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,20 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const plugins = [commonjs(), nodeResolve()];
 
 module.exports = [
-    {
-        input: 'build/lib/expectTheUnexpected.js',
-        external: ['unexpected'],
-        output: {
-            file: 'index.js',
-            name: 'expect',
-            exports: 'default',
-            format: 'umd',
-            sourcemap: true,
-            strict: false,
-            globals: {
-                unexpected: 'weknowhow.expect'
-            }
-        },
-        plugins
-    }
+  {
+    input: 'build/lib/expectTheUnexpected.js',
+    external: ['unexpected'],
+    output: {
+      file: 'index.js',
+      name: 'expect',
+      exports: 'default',
+      format: 'umd',
+      sourcemap: true,
+      strict: false,
+      globals: {
+        unexpected: 'weknowhow.expect'
+      }
+    },
+    plugins
+  }
 ];

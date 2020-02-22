db7e7cf224ef6d7528d6a039e05ec21a52ddb818
Alex J Burke <alex@alexjeffburke.com>
2020-02-22 22:35:53 +0100

    Test the top level export.

---
+++ test/cupOfUnexpected.spec.js
@@ -0,0 +1,48 @@
+var unexpected = require('unexpected');
+
+var cupOfUnexpected = require('../lib/cupOfUnexpected');
+
+describe('cupOfUnexpected', () => {
+  describe('assert', () => {
+    it('should throw unsupported message', () => {
+      unexpected(
+        () => {
+          // eslint-disable-next-line no-unused-expressions
+          cupOfUnexpected.assert;
+        },
+        'to throw',
+        'assert interface is not supported.'
+      );
+    });
+  });
+
+  describe('expect', () => {
+    it('should return the implementation', () => {
+      unexpected(cupOfUnexpected.expect, 'to be a function');
+    });
+  });
+
+  describe('should()', () => {
+    it('should throw unsupported message', () => {
+      unexpected(
+        () => {
+          cupOfUnexpected.should();
+        },
+        'to throw',
+        'should interface is no supported.'
+      );
+    });
+  });
+
+  describe('use()', () => {
+    it('should throw unsupported message', () => {
+      unexpected(
+        () => {
+          cupOfUnexpected.use();
+        },
+        'to throw',
+        'use() of plugins is not supported.'
+      );
+    });
+  });
+});
